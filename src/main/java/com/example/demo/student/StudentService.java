1b570d895cb32efec8c664306d4068e5c716ff3a
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-07 02:20:18 +0200

    Created StudentRepository to be able to make querys on table students. Created StudentConfig to save two students on db. Service now calls repository students.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,22 +9,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "mariam",
-                        "mariam@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                ),
-                new Student(
-                        2L,
-                        "mariam2",
-                        "mariam@gmail.com2",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        212
-                )
-        );
+        return studentRepository.findAll();
     }
 }
